9c02e39e08c92fc28c897eb4ea9b960476f01eac
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-06-27 12:52:01 +0530

    Update Revanced-Experiment.sh

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -24,3 +24,10 @@ patch "telegram-web-version" "revanced-experiments"
 get_patches_key "instagram-revanced-experiments"
 get_apk "com.instagram.android" "instagram-arm64-v8a" "instagram-instagram" "instagram/instagram-instagram/instagram-instagram" "arm64-v8a" "nodpi"
 patch "instagram-arm64-v8a" "revanced-experiments"
+
+revanced_dl
+	# Patch Spotjfy Arm64-v8a
+	get_patches_key "Spotjfy-revanced"
+	j="i"
+	get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music" "Bundle"
+	patch "spotjfy-arm64-v8a" "revanced"
