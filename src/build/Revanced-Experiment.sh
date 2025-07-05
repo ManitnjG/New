9680832f8f43dd15400f7f09109d996ddaba0d68
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-07-05 17:25:45 +0530

    Update Revanced-Experiment.sh

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -14,6 +14,7 @@ dl_gh "revanced-cli" "revanced" "latest"
 # Patch Instagram:
 get_patches_key "instagram-revanced-experiments"
 wget "https://dw.uptodown.net/dwn/RvVkii134Riphftvun7hQBZyU0aCwJjJMFI3FD3XyiS2lMRqLHnjoYVPOf9Zv6sqh9hWBh1e-BijEGTweThzhcmGIU__puRCPMm0O7uQG6VeEbPYkbuIuZY9tiaRt_ge/U3rjlKgKci5ogZPSMyOd3bSK73JbNgkVyVFEIZnShStU9SHVUfcDMJaeWS844KfESrkg88L3BUHnHW_xPMfSUsRZpfnKnIcUI9vO3rdMLChdgxvx4_qvszitOKDQBteW/s4AS_Ec1brMMvP9j-Hz0MotwnkpDNdoALKentC8QLvksQM1PhQPM0ORKFB7YZNN_h89Y6w9gA1CGiZYFYdZUHTI1JHW47Lwo651cd3cFp2s=/uptodown-com.instagram.android.apk"
+mv "uptodown-com.instagram.android.apk" "instagram-arm64-v8a.apk"
 patch "instagram-arm64-v8a" "revanced-experiments"
 revanced_dl(){
 	dl_gh "revanced-patches revanced-cli" "revanced" "latest"
