2aa3c33efc9a42cc92dce3945206e3d36132edb3
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-07-07 15:32:06 +0530

    Update Revanced-Experiment.sh

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -13,8 +13,8 @@ dl_gh "revanced-cli" "revanced" "latest"
 
 # Patch Instagram:
 get_patches_key "instagram-revanced-experiments"
-wget "https://dw.uptodown.net/dwn/RvVkii134Riphftvun7hQBZyU0aCwJjJMFI3FD3XyiS2lMRqLHnjoYVPOf9Zv6sqh9hWBh1e-BijEGTweThzhcmGIU__puRCPMm0O7uQG6VeEbPYkbuIuZY9tiaRt_ge/U3rjlKgKci5ogZPSMyOd3bSK73JbNgkVyVFEIZnShStU9SHVUfcDMJaeWS844KfESrkg88L3BUHnHW_xPMfSUsRZpfnKnIcUI9vO3rdMLChdgxvx4_qvszitOKDQBteW/s4AS_Ec1brMMvP9j-Hz0MotwnkpDNdoALKentC8QLvksQM1PhQPM0ORKFB7YZNN_h89Y6w9gA1CGiZYFYdZUHTI1JHW47Lwo651cd3cFp2s=/uptodown-com.instagram.android.apk"
-mv "uptodown-com.instagram.android.apk" "instagram-arm64-v8a.apk"
+wget "https://eb5e7388c3df147b74dd2379b7cf8323.r2.cloudflarestorage.com/downloadprod/wp-content/uploads/2025/06/86/685baaaa7dd50/com.instagram.android_386.0.0.12.84-379005219_minAPI28%28arm64-v8a%29%28nodpi%29_apkmirror.com.apk?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=72a5ba3a0b8a601e535d5525f12f8177%2F20250707%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20250707T100045Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=7c978569f03828223397c35c62281fcd7e4d734456ecf475e5c28d70cada9939"
+mv "*.apk" "instagram-arm64-v8a.apk"
 patch "instagram-arm64-v8a" "revanced-experiments"
 revanced_dl(){
 	dl_gh "revanced-patches revanced-cli" "revanced" "latest"
