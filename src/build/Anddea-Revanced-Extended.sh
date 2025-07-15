0ea5015fad71195d34d4676fe4ab1937d5643c79
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-07-15 12:22:29 +0530

    Update Anddea-Revanced-Extended.sh

--- src/build/Anddea-Revanced-Extended.sh
+++ src/build/Anddea-Revanced-Extended.sh
@@ -3,6 +3,7 @@
 source src/build/utils.sh
 # Download requirements
 dl_gh "revanced-patches" "anddea"
+dl_gh "revanced-cli" "revanced" "latest"
 get_patches_key "Spotjfy-anddea"
 j="i"
 get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
